daf45d918dcb19da14638a5c5b095062449024fa
Bayrjargal Namnansuren <bayrjargal0508@gmail.com>
2025-12-03 16:22:28 +0800

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import ParticlesBackground from "@/components/context/particles-bg";
 import Header from "@/components/context/header";
-import CanvasComponent from "@/components/context/cursor";
-import BodyDetails from "@/components/details/body";
 
 export default function Home() {
   return (
