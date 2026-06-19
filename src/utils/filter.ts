1a9313cd432c6571bf09b8a61b75ce0a9c1eac9f
Bayrjargal Namnansuren <158271288+bayrjargal0508@users.noreply.github.com>
2026-06-19 12:47:37 +0800

    changes

--- src/utils/filter.ts
+++
@@ -1,5 +0,0 @@
-// utils/filter.ts
-export const isBlockedUrl = (url: string): boolean => {
-  const blockedDomains = ['example-porn.com', 'socialmedia.com'];
-  return blockedDomains.some(domain => url.includes(domain));
-};
